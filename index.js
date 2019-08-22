cd4f0db1e66199f12a7ecbb9fea6af56b0f01cb2
portiaportia <portiaplante@gmail.com>
2019-08-22 12:28:12 -0400

    ds

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let server = http.createServer(function (req, res) {
         res.writeHead(200, {
             'Content-Type': 'text/plain'
         });
-        res.write('hello portia!', 'utf-8');
+        res.write(JSON.stringify([1,2,3]));
         res.end();
  
     });
